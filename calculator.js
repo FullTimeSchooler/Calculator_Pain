a5317c34b9b3d0de2db7cf9358e3c4afbdf0b8b9
FullTimeSchooler <143832567+FullTimeSchooler@users.noreply.github.com>
2024-01-25 23:27:35 -0800

    added some js and also customized calculator, however calculator doesn't really work

--- calculator.js
+++ calculator.js
@@ -1,4 +1,17 @@
-function addition(boo){
-    
-console.log(addition)
+function display(value) {
+    document.getElementById("answer").value += value
+}
+
+function clr() {
+    document.getElementById("answer").value = ""
+}
+
+function math(equation) {
+    if ($("one").val() === "1" || $("two").val === "2"
+        || $("three").val() === "3" || $(four).val() === "4"
+        || $(five).val() === "5" || $(six).val() === "6"
+        || $(seven).val() === "7" || $(eight).val === "8"
+        || $(nine).val === "9" || $(plus).val === "+" || $(minus).val() === "-"
+        || $(multiply).val === "*" || $(division).val === "/" || $(decimal).val() === ".")
+        document.getElementById("answer").value += variables;
 }
